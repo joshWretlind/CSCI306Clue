2cf3a4f2068b0db7de3cde896fc5259bf3383b44
unknown <jwretlin@.adit.mines.edu>
2012-10-04 09:17:24 -0600

    Wrote a test

--- src/IntBoard.java
+++ src/IntBoard.java
@@ -1,4 +1,33 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 
 public class IntBoard {
+
+	public IntBoard(){
+		
+	}
+	
+	public void calcAdjacencies(){
+		
+	}
+	
+	public void calcTarget(int start, int steps){
+		
+	}
+	
+	public Set getTargets(){
+		return new TreeSet();
+	}
+	
+	public LinkedList getAdjList(){
+		return new LinkedList();
+	}
+	
+	public static int calcIndex(){
+		return 0;
+	}
 
 }
--- src/IntBoardTests.java
+++ src/IntBoardTests.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.Assert;
@@ -9,16 +10,28 @@ import junit.framework.TestCase;
 public class IntBoardTests extends TestCase {
 	IntBoard board;
 	
+	@Before
+	public void setup(){
+		board = new IntBoard();
+	}
+	
 	public IntBoardTests(){
 		board = new IntBoard();
 	}
 	
 	@Test
 	public void testAdjacency(){
-		LinkedList testList = (LinkedList) board.getAdjList();
+		LinkedList testList =.getAdjList();
 		Assert.assertTrue(!testList.isEmpty());
 		Assert.assertTrue(testList.contains(1));
 		Assert.assertTrue(testList.contains(4));
+		Assert.assertTrue(testList.size() == 2);
 		
 	}
+	
+	@Test
+	public void testIndex(){
+		
+	}
+	
 }
