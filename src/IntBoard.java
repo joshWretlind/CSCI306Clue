c811a7c287280501223737ffe5dd3e9a540be15f
Josh Wretlind <>
2012-10-08 16:05:55 -0600

    Retooled a fair amount of code

--- src/IntBoard.java
+++ src/IntBoard.java
@@ -1,3 +1,4 @@
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -5,98 +6,85 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class IntBoard {
-	private int row = 4;
-	private int col = 4;
-	private HashMap<Integer, LinkedList<Integer>> adj
-		= new HashMap<Integer, LinkedList<Integer>>();
-	private LinkedList<Integer> x;
-	private boolean visited [] = new boolean [row*row];
-	
-	public IntBoard(){
-		for (int i=0; i <Math.pow(row,2); i++) {
-			visited[i] = false;
-		}
-	}
-	
-	public void calcAdjacencies(){
-		for (int i=0; i <Math.pow(row,2); i++) {
-			x = new LinkedList<Integer>();
-			if (i==0) {
-				x.add(1);
-				x.add(row);
-				adj.put(i, x);
-			}
-			else if (i==(Math.pow(row, 2)-1)){
-				x.add(i-1);
-				x.add(i-row);
-				adj.put(i, x); 
-			}
-			else if (i==(Math.pow(row, 2)-row)) {
-				x.add(i+1);
-				x.add(i-row);
-				adj.put(i, x);
-			}
-			else if (i==(row-1)) {
-				x.add(i-1);
-				x.add(i+row);
-				adj.put(i, x);
-			}
-			else if (i%row==0 && i!=(Math.pow(row, 2)-row)) {
-				x.add(i+1);
-				x.add(i+row);
-				x.add(i-row);
-				adj.put(i, x); 
-			}
-			else if ((i+1)%row==0 && i!=(row-1)) {
-				x.add(i-1);
-				x.add(i+row);
-				x.add(i-row);
-				adj.put(i, x);
-			}
-			else if (i>0 && i<row-1) {
-				x.add(i+1);
-				x.add(i-1);
-				x.add(i+row);
-				adj.put(i, x);
-			}
-			else if (i<Math.pow(row, 2) && i>Math.pow(row, 2)-row) {
-				x.add(i+1);
-				x.add(i-1);
-				x.add(i-row);
-				adj.put(i, x);
-			}
-			else {
-				x.add(i+1);
-				x.add(i-1);
-				x.add(i+row);
-				x.add(i-row);
-				adj.put(i, x);	 
-			}
-			
-		}
-	}
-	
-	public void calcTargets(int start, int steps){
-		
-	}
-	
-	public TreeSet getTargets(){
-		return new TreeSet();
-	}
-	
-	public LinkedList getAdjList(int indx){
-		return adj.get(indx);
-	}
-	
-	public int calcIndex(int r, int c){
-		return row*r+c;
-	}
-	
-	public static void main(String[] args) {
-		IntBoard ib = new IntBoard();
-		ib.calcAdjacencies();
-		ib.calcTargets(0, 2);
-	}
 
+    private int row = 4;
+    private int col = 4;
+    // Note: we should probably, for the sake of good design, use a Board Cell object instead of Integers here. That way we can store both the i and j position and clean up the adjacency list code quite a bit.
+    private HashMap<Integer, List<Integer>> adj = new HashMap<>();
+    private List adjList;
+    private List targets;
+    private boolean visited[][];
+
+    public IntBoard() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    public void calcAdjacencies() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i != j) {
+                    boolean top = false;
+                    boolean bottom = false;
+                    boolean right = false;
+                    boolean left = false;
+                    if ((i + 1) == row) {
+                        right = true;
+                    }
+                    if ((i - 1) == -1) {
+                        left = true;
+                    }
+                    if (j - 1 == -1) {
+                        top = true;
+                    }
+                    if ((j + 1) == col) {
+                        bottom = true;
+                    }
+                    adjList = new LinkedList<Integer>();
+                    if (!right) {
+                        adjList.add(4 * j + i + 1);
+                    }
+                    if (!left) {
+                        adjList.add(4 * j + i - 1);
+                    }
+                    if (!top) {
+                        adjList.add(4 * (j - 1) + i);
+                    }
+                    if (!bottom) {
+                        adjList.add(4 * (j + 1) + i);
+                    }
+                    adj.put(4*j + i, adjList);
+                }
+
+            }
+        }
+    }
+
+    public void calcTargets(int rowT, int colT, int steps) {
+        for (int i = 0; i < steps; i++) {
+            for (Object o : getAdjList(start)) {
+            }
+        }
+    }
+
+    public TreeSet getTargets() {
+        return new TreeSet();
+    }
+
+    public LinkedList getAdjList(int indx) {
+        return (LinkedList) adj.get(indx);
+    }
+
+    public int calcIndex(int r, int c) {
+        return row * r + c;
+    }
+
+    public static void main(String[] args) {
+        IntBoard ib = new IntBoard();
+        ib.calcAdjacencies();
+        ib.calcTargets(0, 2);
+    }
 }
-
